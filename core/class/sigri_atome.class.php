91a7492200a449eacb82383d4ee77b5ac8ad75fd
Sigri44 <sigri44@hotmail.fr>
2019-01-26 16:24:02 +0100

    Fix error cookie login

--- core/class/sigri_atome.class.php
+++ core/class/sigri_atome.class.php
@@ -172,7 +172,7 @@
 				CURLOPT_TIMEOUT => 30,
 				CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
 				CURLOPT_CUSTOMREQUEST => "POST",
-				CURLOPT_POSTFIELDS => "{\"email\": \"$login\",\"plainPassword\": \"$password\"}",
+				CURLOPT_POSTFIELDS => "{\"email\": \"".$login."\",\"plainPassword\": \"".$password."\"}",
 				CURLOPT_HTTPHEADER => array(
 					"Content-Type: application/json",
 					"cache-control: no-cache"

0b3b6c0bb451142a308f9b203478c12e655f15a9
Sigri44 <sigri44@hotmail.fr>
2019-01-26 19:01:46 +0100

    BDD export

--- core/class/sigri_atome.class.php
+++ core/class/sigri_atome.class.php
@@ -262,7 +262,9 @@
 							// Enregistrement de l'heure dans la BDD
 							log::add('sigri_atome', 'debug', 'Enregistrement dans la BDD en cours de l\'heure : '.$i);
 							$sql = 'INSERT INTO sigri_atome_hour (hour, total_consumption, index_hp, index_hc, cost_hp, cost_hc) VALUES (\''.$datetime.'\', \''.$totalConsumption.'\', \''.$indexHP.'\', \''.$indexHC.'\', \''.$costHP.'\', \''.$costHC.'\')ON DUPLICATE KEY UPDATE total_consumption='.$totalConsumption.', index_hp='.$indexHP.', index_hc='.$indexHC.', cost_hp='.$costHP.', cost_hc='.$costHC.'';
+							log::add('sigri_atome', 'debug', 'RQT $sql : ' . $sql);
 							$result = DB::Prepare($sql, array(), DB::FETCH_TYPE_ALL);
+							log::add('sigri_atome', 'debug', '$result : ' . $result);
 							$nbenreg = count($result);
 							log::add('sigri_atome', 'debug', 'Nombre d\'enregistrement "heure" effectués avec succès : '.$nbenreg);
 						}

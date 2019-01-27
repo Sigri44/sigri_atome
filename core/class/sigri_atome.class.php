bb69fb2c7afc6f7c832669ab4d3f3ce6d913e105
Sigri44 <sigri44@hotmail.fr>
2019-01-27 16:58:25 +0100

    Test ajout fonction addFunctionHistory

--- core/class/sigri_atome.class.php
+++ core/class/sigri_atome.class.php
@@ -91,6 +91,8 @@
 						$period = "day";
 						$eqLogic->Call_Atome_API($json_connection, $period);
 					}
+				} else {
+					log::add('sigri_atome', 'error', 'Aucun équipement n\'est configuré/activé !');
 				}
 			}
 		}
@@ -101,15 +103,15 @@
 			foreach ($eqLogics as $eqLogic) {
 				if ($eqLogic->getIsEnable() == 1) {
 					if (!empty($eqLogic->getConfiguration('identifiant')) && !empty($eqLogic->getConfiguration('password'))) {
-						/*
 						log::add('sigri_atome', 'debug', 'Debug avant login');
 						log::add('sigri_atome', 'debug', 'Login : '.$eqLogic->getConfiguration('identifiant'));
 						log::add('sigri_atome', 'debug', 'Password : '.$eqLogic->getConfiguration('password'));
-						*/
 						$json_connection = $eqLogic->Call_Atome_Login($eqLogic->getConfiguration('identifiant'), $eqLogic->getConfiguration('password'));
 						$period = "month";
 						$eqLogic->Call_Atome_API($json_connection, $period);
 					}
+				} else {
+					log::add('sigri_atome', 'error', 'Aucun équipement n\'est configuré/activé !');
 				}
 			}
 		}
